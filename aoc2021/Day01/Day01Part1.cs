3547feba353f637b08040343c13f0c94292cd6e5
korla <bengtsson.johannes@gmail.com>
2021-12-02 17:30:17 +0100

    Day01Part1

---
+++ aoc2021/Day01/Day01Part1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace aoc2021.Day1;
+
+public class Day01Part1
+{
+    private int Run(IEnumerable<string> data)
+    {
+        var values = data
+            .Select(int.Parse)
+            .ToList();
+        return values
+            .Zip(
+                values.Skip(1),
+                (first, second) => second > first
+            )
+            .Count(a => a);
+    }
+    
+    private class Day01Part1Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day01/testdata.txt");
+            var sut = new Day01Part1();
+            Assert.AreEqual(7, sut.Run(data));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day01/data.txt");
+            var sut = new Day01Part1();
+            Assert.AreEqual(1446, sut.Run(data));
+        }
+    }
+}
