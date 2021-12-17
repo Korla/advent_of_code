4a67a111692bc3df45a6523b4c1bf2c6109e132c
korla <bengtsson.johannes@gmail.com>
2021-12-17 16:47:09 +0100

    Day17Part2

---
+++ aoc2021/Day17Part2/Day17Part2.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace aoc2021.Day17Part2;
+
+public class Day17Part2
+{
+    private int Run(int xMin, int xMax, int yMin, int yMax)
+    {
+        var maxValue = 1000;
+        var count = 0;
+        for (var y = yMin - 1; y < maxValue; y++)
+        {
+            for (var x = 0; x < xMax + 1; x++)
+            {
+                if (CheckIfHits(x, y))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+        bool CheckIfHits(int xVel, int yVel)
+        {
+            var yPos = 0;
+            var xPos = 0;
+            while (yPos >= yMin && xPos <= xMax)
+            {
+                xPos += xVel;
+                yPos += yVel;
+                yVel -= 1;
+                xVel = Math.Max(xVel - 1, 0);
+                if (xMin <= xPos && xPos <= xMax && yMin <= yPos && yPos <= yMax)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private class Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var sut = new Day17Part2();
+            Assert.AreEqual(112, sut.Run(20, 30, -10, -5));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var sut = new Day17Part2();
+            Assert.AreEqual(2032, sut.Run(211, 232, -124, -69));
+        }
+    }
+}
