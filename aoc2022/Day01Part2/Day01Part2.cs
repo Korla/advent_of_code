712b4f6aa1043589abd75038bf5df40cf1b4a1a2
Johannes Bengtsson <johannes.bengtsson@tradera.com>
2022-12-01 07:56:03 +0100

    2022 Day01Part2

---
+++ aoc2022/Day01Part2/Day01Part2.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace aoc2022.Day01Part2;
+
+public class Day01Part2
+{
+    private int Run(IEnumerable<string> data)
+    {
+        var values = data
+            .ToList();
+        var elves = new List<int>();
+        var current = 0;
+        foreach (var value in values)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                elves.Add(current);
+                current = 0;
+            }
+            else
+            {
+                current += int.Parse(value);
+            }
+        }
+        elves.Add(current);
+
+        return elves.OrderByDescending(a => a).Take(3).Sum();
+    }
+    
+    private class Day01Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day01Part2/testdata.txt");
+            var sut = new Day01Part2();
+            Assert.AreEqual(45000, sut.Run(data));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day01Part2/data.txt");
+            var sut = new Day01Part2();
+            Assert.AreEqual(205615, sut.Run(data));
+        }
+    }
+}
