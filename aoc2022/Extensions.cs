9c384d060e1888141699cb8934f25c6354686b52
Johannes Bengtsson <johannes.bengtsson@tradera.com>
2022-12-09 13:43:38 +0100

    Print vectors method

--- aoc2022/Extensions.cs
+++ aoc2022/Extensions.cs
@@ -11,6 +11,26 @@ public static class VectorExtensions
     public static Vector Add(this Vector vector, Vector other) => new(vector.X + other.X, vector.Y + other.Y);
     public static Vector Subtract(this Vector vector, Vector other) => new(vector.X - other.X, vector.Y - other.Y);
     public static double Length(this Vector vector) => Math.Sqrt(Math.Pow(Math.Abs(vector.X),2) + Math.Pow(Math.Abs(vector.Y),2));
+
+    public static void Print(this IEnumerable<Vector> vectors)
+    {
+        var xMin = vectors.Select(v => v.X).Min();
+        var xMax = vectors.Select(v => v.X).Max();
+        var yMin = vectors.Select(v => v.Y).Min();
+        var yMax = vectors.Select(v => v.Y).Max();
+        var xRange = Enumerable.Range(xMin, xMax-xMin + 1).ToArray();
+        var yRange = Enumerable.Range(yMin, yMax-yMin + 1).ToArray();
+        foreach (var x in xRange)
+        {
+            foreach (var y in yRange)
+            {
+                var vector = new Vector(x, y);
+                Console.Write(vectors.Any(v => v == vector) ? "X" : ".");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("------------------------------------");
+    }
 }
 
 public static class EnumerableExtensions
