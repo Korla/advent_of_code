f398ca3cc51420b61f1114d9310f8a8bd92db2a1
Johannes Bengtsson <johannes.bengtsson@tradera.com>
2022-12-13 18:09:36 +0100

    int.Limit

--- Utils/IntExtensions.cs
+++ Utils/IntExtensions.cs
@@ -3,4 +3,5 @@
 public static class IntExtensions
 {
     public static bool IsBetweenInclusive(this int source, int min, int max) => source >= min && source <= max;
+    public static int Limit(this int source, int min, int max) => Math.Min(Math.Max(min, source), max);
 }
