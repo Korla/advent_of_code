eb5a32999d131e35cc389cda03014b7f32b70e81
Johannes Bengtsson <johannes.bengtsson@tradera.com>
2022-12-14 08:27:22 +0100

    Utils.Pairwise

--- Utils/EnumerableExtensions.cs
+++ Utils/EnumerableExtensions.cs
@@ -31,4 +31,16 @@ public static class EnumerableExtensions
             i++;
         }
     }
+
+    public static IEnumerable<TResult> Pairwise<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TSource, TResult> resultSelector)
+    {
+        var previous = default(TSource);
+
+        using var it = source.GetEnumerator();
+        if (it.MoveNext())
+            previous = it.Current;
+
+        while (it.MoveNext())
+            yield return resultSelector(previous, previous = it.Current);
+    }
 }
