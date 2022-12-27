328ff7e3ab973f146eb96aa33ba9678e41dd464b
korla <bengtsson.johannes@gmail.com>
2022-12-27 18:45:43 +0100

    Print vector coordinates

--- Utils/Vector.cs
+++ Utils/Vector.cs
@@ -17,14 +17,33 @@ public static class VectorExtensions
 
     public static void Print(this IEnumerable<Vector> vectors)
     {
+        vectors = vectors.ToList();
         var xMin = vectors.Select(v => v.X).Min();
         var xMax = vectors.Select(v => v.X).Max();
         var yMin = vectors.Select(v => v.Y).Min();
         var yMax = vectors.Select(v => v.Y).Max();
         var xRange = Enumerable.Range(xMin, xMax-xMin + 1).ToArray();
         var yRange = Enumerable.Range(yMin, yMax-yMin + 1).ToArray();
-        foreach (var y in yRange)
+        var xNumberLength = Math.Max(Math.Abs(xMax), Math.Abs(xMin)).ToString().Length;
+        var yNumberLength = Math.Max(Math.Abs(yMax), Math.Abs(yMin)).ToString().Length;
+        foreach (var i in Enumerable.Range(0, xNumberLength))
         {
+            foreach (var i1 in Enumerable.Range(0, yNumberLength))
+            {
+                Console.Write(" ");
+            }
+            Console.Write(" ");
+
+            foreach (var x in xRange)
+            {
+                Console.Write(x.ToString()[i]);
+            }
+            Console.WriteLine();
+        }
+        foreach (var y in yRange.Reverse())
+        {
+            Console.Write(y.ToString());
+            Console.Write(" ");
             foreach (var x in xRange)
             {
                 var vector = new Vector(x, y);
