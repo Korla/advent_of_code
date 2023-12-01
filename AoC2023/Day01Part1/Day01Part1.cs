6b580c424f1914e5ebdd7587406f25e04e04cd8f
korla <bengtsson.johannes@gmail.com>
2023-12-01 08:38:43 +0100

    2023 Day01Part1

---
+++ AoC2023/Day01Part1/Day01Part1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using NUnit.Framework;
+
+namespace AoC2023.Day01Part1;
+
+public class Day01Part1
+{
+    private int Run(IEnumerable<string> data)
+    {
+         return data
+            .Sum(line =>
+                {
+                    var integers = line
+                        .Where(c => int.TryParse(c.ToString(), out _))
+                        .ToList();
+                    return int.Parse($"{integers.First()}{integers.Last()}");
+                }
+            );
+    }
+    
+    private class Day01Part1Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day01Part1/testdata.txt");
+            var sut = new Day01Part1();
+            Assert.AreEqual(142, sut.Run(data));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day01Part1/data.txt");
+            var sut = new Day01Part1();
+            Assert.AreEqual(54927, sut.Run(data));
+        }
+    }
+}
