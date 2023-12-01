d29d63084d600d6b9a01e35cd832cf202de2ed64
korla <bengtsson.johannes@gmail.com>
2023-12-01 09:06:39 +0100

    2023 Day01Part2

---
+++ AoC2023/Day01Part2/Day01Part2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using NUnit.Framework;
+using Utils;
+
+namespace AoC2022.Day01Part2;
+
+public class Day01Part2
+{
+    private readonly Dictionary<string, string> _digitMap = new()
+    {
+        {"1", "1"},
+        {"2", "2"},
+        {"3", "3"},
+        {"4", "4"},
+        {"5", "5"},
+        {"6", "6"},
+        {"7", "7"},
+        {"8", "8"},
+        {"9", "9"},
+        {"one", "1"},
+        {"two", "2"},
+        {"three", "3"},
+        {"four", "4"},
+        {"five", "5"},
+        {"six", "6"},
+        {"seven", "7"},
+        {"eight", "8"},
+        {"nine", "9"},
+    };
+
+    private int Run(IEnumerable<string> data) =>
+        data
+            .Select(line => line
+                .Select((_, i) => line[i..])
+                .Select(substring => _digitMap.FirstOrDefault(d => substring.StartsWith(d.Key)).Value)
+                .Where(v => v != null)
+                .ToList()
+            )
+            .Sum(integers => int.Parse($"{integers.First()}{integers.Last()}"));
+
+    private class Day01Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day01Part2/testdata.txt");
+            var sut = new Day01Part2();
+            Assert.AreEqual(281, sut.Run(data));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day01Part2/data.txt");
+            var sut = new Day01Part2();
+            Assert.AreEqual(54581, sut.Run(data));
+        }
+    }
+}
