fc6ea94dedaf36350d2c93335c63d64e4e0a4f5a
korla <bengtsson.johannes@gmail.com>
2023-12-04 20:47:07 +0100

    Template for creating a new day

---
+++ AoCTemplate/DayXXPartYY/DayXXPartYY.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace AoC2023.DayXXPartYY;
+
+public class DayXXPartYY
+{
+  private int Run(IEnumerable<string> data)
+  {
+    return -1;
+  }
+    
+  private class DayXXPartYYTests
+  {
+    [Test]
+    public void TestData()
+    {
+      var data = File.ReadAllLines(@"DayXXPartYY/testdata.txt");
+      var sut = new DayXXPartYY();
+      Assert.AreEqual(0, sut.Run(data));
+    }
+    
+    [Test]
+    public void Data()
+    {
+      var data = File.ReadAllLines(@"DayXXPartYY/data.txt");
+      var sut = new DayXXPartYY();
+      Assert.AreEqual(0, sut.Run(data));
+    }
+  }
+}
