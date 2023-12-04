4ecb3b3698a23d5625073bc5df0eec7b747fee63
korla <bengtsson.johannes@gmail.com>
2023-12-04 10:04:45 +0100

    2023 Day04Part1

---
+++ AoC2023/Day04Part1/Day04Part1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace AoC2023.Day04Part1;
+
+public class Day04Part1
+{
+    private int Run(IEnumerable<string> data)
+    {
+        return data
+            .Select(row => row.Split(": ").Last().Split(" | "))
+            .Select(numberGroups => numberGroups
+                .Select(numberGroup => numberGroup.Split(" ")
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .Select(number => int.Parse(number.Trim()))
+                ).ToList()
+            )
+            .Sum(numbers => (int)Math.Pow(2, numbers.First().Intersect(numbers.Last()).Count() - 1));
+    }
+    
+    private class Day04Part1Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day04Part1/testdata.txt");
+            var sut = new Day04Part1();
+            Assert.AreEqual(13, sut.Run(data));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day04Part1/data.txt");
+            var sut = new Day04Part1();
+            Assert.AreEqual(18519, sut.Run(data));
+        }
+    }
+}
