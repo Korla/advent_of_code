aabad8021c6dec0624276f07e1e577cebcf4b543
korla <bengtsson.johannes@gmail.com>
2023-12-04 10:52:50 +0100

    IDictionaryExtensions.IncreaseOrAdd

---
+++ Utils/IDictionaryExtensions.cs
@@ -0,0 +1,16 @@
+namespace Utils;
+
+public static class IDictionaryExtensions
+{
+  public static void IncreaseOrAdd<T>(this IDictionary<T, int> source, T key, int count = 1)
+  {
+    if (source.TryGetValue(key, out _))
+    {
+      source[key] += count;
+    }
+    else
+    {
+      source.Add(key, count);
+    }
+  }
+}
