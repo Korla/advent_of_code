c26abbe01f4a5ff4a2683f4ab58df5f791dce08c
korla <bengtsson.johannes@gmail.com>
2023-12-05 11:00:12 +0100

    fixup! Template for creating a new day

--- AoCTemplate/DayXXPartYY/DayXXPartYY.cs
+++ AoCTemplate/DayXXPartYY/DayXXPartYY.cs
@@ -4,27 +4,27 @@ namespace AoC2023.DayXXPartYY;
 
 public class DayXXPartYY
 {
-  private int Run(IEnumerable<string> data)
-  {
-    return -1;
-  }
-    
-  private class DayXXPartYYTests
-  {
-    [Test]
-    public void TestData()
+    private int Run(IEnumerable<string> data)
     {
-      var data = File.ReadAllLines(@"DayXXPartYY/testdata.txt");
-      var sut = new DayXXPartYY();
-      Assert.AreEqual(0, sut.Run(data));
+        return -1;
     }
-    
-    [Test]
-    public void Data()
+      
+    private class DayXXPartYYTests
     {
-      var data = File.ReadAllLines(@"DayXXPartYY/data.txt");
-      var sut = new DayXXPartYY();
-      Assert.AreEqual(0, sut.Run(data));
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"DayXXPartYY/testdata.txt");
+            var sut = new DayXXPartYY();
+            Assert.AreEqual(0, sut.Run(data));
+        }
+
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"DayXXPartYY/data.txt");
+            var sut = new DayXXPartYY();
+            Assert.AreEqual(0, sut.Run(data));
+        }
     }
-  }
 }
