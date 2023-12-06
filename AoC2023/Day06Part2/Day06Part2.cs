cf2b09cdfb29785afabb0dd67426c61e3e8402fb
korla <bengtsson.johannes@gmail.com>
2023-12-06 11:45:13 +0100

    2023 Day06Part2

---
+++ AoC2023/Day06Part2/Day06Part2.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using Utils;
+
+namespace AoC2023.Day06Part2;
+
+public class Day06Part2
+{
+    // Time:      7  15   30
+    // Distance:  9  40  200
+    // Time:        60     94     78     82
+    // Distance:   475   2138   1015   1650
+    private double Run(double time, double distance)
+    {
+        var timePulledBack = 0;
+        while (true)
+        {
+            var distanceRun = timePulledBack * (time - timePulledBack);
+            if (distanceRun > distance)
+            {
+                return time + 1 - 2 * timePulledBack;
+            }
+            timePulledBack++;
+        }
+    }
+      
+    private class Day06Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            // Time:      7  15   30
+            // Distance:  9  40  200
+            var sut = new Day06Part2();
+            Assert.AreEqual(71503, sut.Run(71530, 940200));
+        }
+
+        [Test]
+        public void Data()
+        {
+            // Time:        60     94     78     82
+            // Distance:   475   2138   1015   1650
+            var sut = new Day06Part2();
+            Assert.AreEqual(42588603, sut.Run(60947882, 475213810151650));
+        }
+    }
+}
