ed92cf1eb5470a98764c2b276ea0c8454dd00630
korla <bengtsson.johannes@gmail.com>
2023-12-19 22:14:14 +0100

    2023 Day18Part2

---
+++ AoC2023/Day18Part2/Day18Part2.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using Utils;
+
+namespace AoC2023.Day18Part2;
+
+public class Day18Part2
+{
+    private long Run(IEnumerable<string> data)
+    {
+        var res = data.Select(s => s.Split(" ").Last().Skip(2).ToList())
+            .Aggregate(
+                (loop: new HashSet<Vector>(), current: Vector.Origo, perimeter: 0L),
+                (prev, curr) =>
+                {
+                    var enumerable = curr.Take(5);
+                    var length = long.Parse(string.Join("", enumerable), System.Globalization.NumberStyles.HexNumber);
+                    var direction = Vector.CardinalDirections[(curr[5] + 1) % 4];
+                    var (loop, current, perimeter) = prev;
+
+                    perimeter += length;
+
+                    loop.Add(current);
+                    for (var i = 0L; i < length; i++)
+                    {
+                        current = current.Add(direction);
+                    }
+
+                    loop.Add(current);
+
+                    return (loop, current, perimeter);
+                }
+            );
+        var (loop, _, perimeter) = res;
+            
+        return loop.Pairwise((a, b) => (long)a.X * b.Y - (long)a.Y * b.X).Sum() / 2 + perimeter / 2 + 1;
+    }
+
+    private class Day18Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines(@"Day18Part2/testdata.txt");
+            var sut = new Day18Part2();
+            Assert.AreEqual(952408144115, sut.Run(data));
+        }
+
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines(@"Day18Part2/data.txt");
+            var sut = new Day18Part2();
+            Assert.AreEqual(45757884535661, sut.Run(data));
+        }
+    }
+}
