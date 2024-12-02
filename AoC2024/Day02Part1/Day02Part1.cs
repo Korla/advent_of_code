d1551e4f594ddd22b07ff1b903adc96b0f2e29f3
Johannes Bengtsson <bengtsson.johannes@gmail.com>
2024-12-02 08:17:54 +0100

    2024 Day02Part1

---
+++ AoC2024/Day02Part1/Day02Part1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Utils;
+
+namespace AoC2024.Day02Part1;
+
+public class Day02Part1
+{
+    private int Run(IEnumerable<string> data)
+    {
+        return data
+            .Count(
+                row =>
+                {
+                    var deltas = row
+                            .Split(" ")
+                            .Select(int.Parse)
+                            .Pairwise((a, b) => b - a);
+                    var isIncreasing = deltas.First() > 0;
+                    return deltas.All(delta =>
+                        isIncreasing ? delta.IsBetweenInclusive(1, 3) : delta.IsBetweenInclusive(-3, -1));
+                }   
+            );
+    }
+    
+    private class Day02Part1Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines("Day02Part1/testdata.txt");
+            var sut = new Day02Part1();
+            Assert.That(sut.Run(data), Is.EqualTo(2));
+        }
+    
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines("Day02Part1/data.txt");
+            var sut = new Day02Part1();
+            Assert.That(sut.Run(data), Is.EqualTo(359));
+        }
+    }
+}
