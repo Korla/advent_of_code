df3be407798fae6d57e577ecdff3d485b4330cc6
korla <bengtsson.johannes@gmail.com>
2024-12-16 22:34:53 +0100

    2024 Day16Part2

---
+++ AoC2024/Day16Part2/Day16Part2.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Utils;
+
+namespace AoC2024.Day16Part2;
+
+public class Day16Part2
+{
+    private record Location(Vector Position, Vector Direction, int Weight);
+
+    private int Run(IEnumerable<string> data)
+    {
+        var memoizedNeighbors = new Dictionary<Location, List<Location>>();
+        var actualStart = Vector.Origo;
+        var actualEnd = Vector.Origo;
+        var maze = new HashSet<Vector>();
+        foreach (var cell in data.SelectMany((row, y) => row.Select((c, x) => (p: new Vector(x, y), c))))
+        {
+            switch (cell.c)
+            {
+                case 'S':
+                    actualStart = cell.p;
+                    maze.Add(cell.p);
+                    break;
+                case '.':
+                    maze.Add(cell.p);
+                    break;
+                case 'E':
+                    actualEnd = cell.p;
+                    maze.Add(cell.p);
+                    break;
+            }
+        }
+
+        var (minWeight, _) = RunDijkstra(new Location(actualStart, Vector.Right, 0), actualEnd, int.MaxValue);
+        var possibleRoutes = new List<Vector>();
+        foreach (var position in maze)
+        {
+            var (firstPartWeight, end) = RunDijkstra(new Location(actualStart, Vector.Right, 0), position, minWeight);
+            if (firstPartWeight > minWeight)
+            {
+                continue;
+            }
+            var (secondPartWeight, _) = RunDijkstra(end, actualEnd, minWeight - firstPartWeight);
+            if (firstPartWeight + secondPartWeight == minWeight)
+            {
+                possibleRoutes.Add(position);
+            }
+        }
+        return possibleRoutes.Distinct().Count() + 1;
+
+        (int weight, Location end) RunDijkstra(Location start, Vector end, int maxWeight)
+        {
+            var dijkstra = new Dijkstra<Location>
+            {
+                target = location => location.Position == end,
+                weight = location => location.Weight,
+                valid = location => maze.Contains(location.Position),
+                neighbors = location =>
+                {
+                    var isCached = memoizedNeighbors.TryGetValue(location, out var neighbors);
+                    if (!isCached)
+                    {
+                        neighbors =
+                        [
+                            location with { Direction = location.Direction.Rotate(), Weight = 1000 },
+                            location with { Direction = location.Direction.RotateACW(), Weight = 1000 },
+                            location with { Position = location.Position.Add(location.Direction), Weight = 1 }
+                        ];
+                        memoizedNeighbors.Add(location, neighbors);
+                    }
+
+                    return neighbors;
+                }
+            };
+            return dijkstra.ComputeWithEnd(start, maxWeight);
+        }
+    }
+
+    private class Day16Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines("Day16Part2/testdata.txt");
+            var sut = new Day16Part2();
+            Assert.That(sut.Run(data), Is.EqualTo(64));
+        }
+
+        [Test]
+        [Explicit]
+        public void Data()
+        {
+            var data = File.ReadAllLines("Day16Part2/data.txt");
+            var sut = new Day16Part2();
+            var actual = sut.Run(data);
+            Assert.That(actual, Is.EqualTo(109496));
+        }
+    }
+}
--- Utils/Dijkstra.cs
+++ Utils/Dijkstra.cs
@@ -38,4 +38,37 @@ public class Dijkstra<T> where T : notnull
         }
         return int.MaxValue;
     }
+
+    public (int, T) ComputeWithEnd(T start, int maxWeight)
+    {
+        var dist = new Dictionary<T, int>
+        {
+            [start] = 0
+        };
+        var seen = new HashSet<T>();
+        var queue = new PriorityQueue<T, int>();
+        queue.Enqueue(start, 0);
+        while (queue.Count > 0)
+        {
+            var cell = queue.Dequeue();
+            if (seen.Contains(cell)) continue;
+            var current = dist[cell];
+            if (current > maxWeight) continue;
+            if (target(cell)) return (current, cell);
+            seen.Add(cell);
+
+            foreach (var neighbor in neighbors(cell))
+            {
+                if (!valid(neighbor)) continue;
+                var newScore = current + weight(neighbor);
+                if (!seen.Contains(neighbor)) queue.Enqueue(neighbor, newScore);
+                var distanceToNeighbor = dist.TryGetValue(neighbor, out var d) ? d : int.MaxValue;
+                if (newScore < distanceToNeighbor)
+                {
+                    dist[neighbor] = newScore;
+                }
+            }
+        }
+        return (int.MaxValue, default)!;
+    }
 }
