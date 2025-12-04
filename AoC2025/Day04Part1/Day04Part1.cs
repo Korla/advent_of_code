f288bfe799209165da63206f42f60fe171feabaa
korla <bengtsson.johannes@gmail.com>
2025-12-04 22:31:22 +0100

    2025 Day04Part1

---
+++ AoC2025/Day04Part1/Day04Part1.cs
@@ -0,0 +1,43 @@
+﻿using Utils;
+
+namespace AoC2025.Day04Part1;
+
+public class Day04Part1
+{
+    private long Run(IEnumerable<string> data)
+    {
+        var grid = data.ToList();
+        var paperRolls = new HashSet<Vector>();
+        for (var y = 0; y < grid.Count; y++)
+        {
+            for (var x = 0; x < grid.Count; x++)
+            {
+                if (grid[y][x] == '@')
+                {
+                    paperRolls.Add(new Vector(x, y));
+                }
+            }
+        }
+
+        return paperRolls.Count(paperRoll => Vector.AllNeighbors.Select(paperRoll.Add).Count(paperRolls.Contains) < 4);
+    }
+
+    private class Day04Part1Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines("Day04Part1/testdata.txt");
+            var sut = new Day04Part1();
+            Assert.That(sut.Run(data), Is.EqualTo(13));
+        }
+
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines("Day04Part1/data.txt");
+            var sut = new Day04Part1();
+            Assert.That(sut.Run(data), Is.EqualTo(1397));
+        }
+    }
+}
