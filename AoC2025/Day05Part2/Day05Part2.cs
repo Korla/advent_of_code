c46ec263706191e4c718e91e45a01e28382ba57d
korla <bengtsson.johannes@gmail.com>
2025-12-05 20:22:17 +0100

    2025 Day05Part2

---
+++ AoC2025/Day05Part2/Day05Part2.cs
@@ -0,0 +1,35 @@
+﻿using Utils;
+
+namespace AoC2025.Day05Part2;
+
+public class Day05Part2
+{
+    private double Run(IEnumerable<string> data)
+    {
+        var dataList = data.ToList();
+        var ranges = dataList.TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line => line.Split('-').Select(long.Parse).ToArray())
+            .Select(line => new Utils.Range(line[0], line[1]))
+            .ToList();
+        return ranges.Simplify().Sum(range => range.Size());
+    }
+
+    private class Day05Part2Tests
+    {
+        [Test]
+        public void TestData()
+        {
+            var data = File.ReadAllLines("Day05Part2/testdata.txt");
+            var sut = new Day05Part2();
+            Assert.That(sut.Run(data), Is.EqualTo(14));
+        }
+
+        [Test]
+        public void Data()
+        {
+            var data = File.ReadAllLines("Day05Part2/data.txt");
+            var sut = new Day05Part2();
+            Assert.That(sut.Run(data), Is.EqualTo(505));
+        }
+    }
+}
